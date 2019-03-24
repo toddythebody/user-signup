9404101d301c2ea5b7e6352accf148c98b8513bf
Todd Sansom <todd.sansom@gmail.com>
2019-03-24 11:42:39 -0500

    Refactoring and an email error bug

--- main.py
+++ main.py
@@ -25,26 +25,30 @@ def index():
     rePassError = ''
     email = request.form["email"]
     emailError = ''
+    validEntry = True
 
     if validate(userName) == False:
         nameError = "User Name must be (3-20) characters with no spaces"
+        validEntry = False
 
     if validate(Pass) == False:
         PassError = "Password must be (3-20) characters with no spaces"
+        validEntry = False
 
     if rePass != Pass:
         rePassError = "Confirmation does not match"
+        validEntry = False
 
     if len(email) == 0:
         pass
     elif validate(email) == False:
         emailError = "Email must be (3-20) characters with no spaces"
-    elif "@" not in email and "." not in email:
-        emailError = "Not a vaild email"
+        validEntry = False
+        if "@" not in email or "." not in email:
+            emailError = "Not a vaild email"
+            validEntry = False
 
-    if nameError != '' or PassError != '' or rePassError != '' or emailError != '':
-        Pass = ''
-        rePass = ''
+    if validEntry == False:
         return render_template("index.html", userName=userName, nameError=nameError, PassError=PassError, rePassError=rePassError, email=email, emailError=emailError)
     else:
         return render_template("home.html", userName=userName)
